483987be974dcaf9cdc0c3dbde9b56d5655ef0a7
sri-harsh <44131849+sri-harsh@users.noreply.github.com>
2022-09-29 22:05:51 -0400

    Delete Lambda directory

--- Lambda/LF0.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    # TODO implement
-    message = "Application under development. Search functionality will be implemented in Assignment 2"
-    return {
-        'statusCode': 200,
-        "messages": [{"type": "unstructured","unstructured": { "text": message }}]
-    }
